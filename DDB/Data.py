74d1b873624674c65b076657ce2783ca7f611578
EzbTYH <wuqiqi1003@gmail.com>
2020-06-13 14:19:04 +0800

    完善DDB.Data.Exporter对高维数据的支持

--- DDB/Data.py
+++ DDB/Data.py
@@ -14,7 +14,7 @@ class Exporter:
     >>> exporter = Exporter(root_path='D:\\J-TEXT')
     >>> data = [0.1, 0.5, 0.3, 0.4, 0.2]
     >>> time = [0, 0.1, 0.2, 0.3, 0.4]
-    >>> exporter.save(data, time, 1051234, '\\ip')
+    >>> exporter.save(1051234,'\\ip',data,time)
     """
     def __init__(self, root_path=None):
         if root_path:
@@ -25,42 +25,41 @@ class Exporter:
         if not os.path.exists(self.hdf5path):
             os.mkdir(self.hdf5path)
 
-    def save(self, data=None, time=None, shot=None, tag=None):
+    def save(self, shot: int, tag: str, data=None, time=None) -> int:
         """
         Save diagnostic data to a hdf5 file
         :param data: Diagnostic data, an array
         :param time: Time axis of diagnostic data
         :param shot: Shot number
         :param tag: Tag of diagnosis
-        :return: nothing
+        :return: code(0:success-1:tag already exits, -2:other failure)
         """
-        data = np.array(data)
-        time = np.array(time)
-        if len(data.shape) != 1 and data.shape[0] > 0:
-            raise ValueError('Parameter \'data\' must be a one-dimensional array!')
-
-        if data.shape[0] != time.shape[0]:
-            raise ValueError('Time doesn\'t match data! Shape of time is {} while shape of data is {}'.
-                             format(time.shape, data.shape))
-
-        if not isinstance(shot, int):
-            raise ValueError('Shot number must be int!')
-
-        path = str(math.floor(shot/100)*100)
+        path = str(math.floor(shot / 100) * 100)
         if not os.path.exists(self.hdf5path + os.sep + path):
             os.mkdir(self.hdf5path + os.sep + path)
         file_path = self.hdf5path + os.sep + path + os.sep + '{}.hdf5'.format(shot)
+
         try:
             h5f = h5py.File(file_path)
+            if tag in h5f.keys():
+                return -1
+            data = np.array(data)
+
+            if time is None:
+                dataset = h5f.create_dataset('/{}'.format(tag), data=data)
+                dataset.attrs.create('SampleRate', -1)
+                dataset.attrs.create('StartTime', -1)
+            else:
+                time = np.array(time)
+                fs = len(data) / (time[-1] - time[0]) if len(time) > 1 else 0
+                start_time = time[0] if len(time) > 1 else 0
+                dataset = h5f.create_dataset('/{}'.format(tag), data=data)
+                dataset.attrs.create('SampleRate', fs)
+                dataset.attrs.create('StartTime', start_time)
+            return 0
         except Exception as e:
             traceback.print_exc(e)
-            os.remove(file_path)
-            h5f = h5py.File(file_path)
-        fs = len(data) / (time[-1] - time[0]) if len(time) > 1 else 0
-        start_time = time[0] if len(time) > 1 else 0
-        dataset = h5f.create_dataset('/{}'.format(tag), data=data)
-        dataset.attrs.create('SampleRate', fs)
-        dataset.attrs.create('StartTime', start_time)
+            return -2
 
 
 class Reader:
@@ -87,7 +86,7 @@ class Reader:
         if not os.path.exists(self.hdf5path):
             raise ValueError('Path {} does n\'t exist.')
 
-    def tags(self, shot=None):
+    def tags(self, shot: int):
         """get tags of a shot
         @:param shot: Shot number, integer
         @:return:Tags of Diagnostic signal
@@ -97,13 +96,13 @@ class Reader:
         try:
             path = str(math.floor(shot / 100) * 100)
             file_path = self.hdf5path + os.sep + path + os.sep + '{}.hdf5'.format(shot)
-            f = h5py.File(file_path, 'r')
-            return [i for i in f.__iter__()]
-        except Exception as err:
-            print('{}'.format(err))
+            h5f = h5py.File(file_path, 'r')
+            return [i for i in h5f.keys()]
+        except Exception as e:
+            print(e)
         return []
 
-    def read_one(self, shot=None, tag=None):
+    def read_one(self, shot: int, tag: str):
         """Read data from hdf5 files
         @:param shot: Shot number, integer
         @:param tag: Tag of diagnostic signal, string
@@ -120,6 +119,8 @@ class Reader:
             st = dataset.attrs.get('StartTime')
             f.close()
             # 重新生成时间
+            if fs < 0:
+                return data
             time_axis = []
             for i in range(data.shape[0]):
                 time_axis.append(st+i*(1/fs))
@@ -145,10 +146,13 @@ class Reader:
                 data = dataset[:]
                 fs = dataset.attrs.get('SampleRate')
                 st = dataset.attrs.get('StartTime')
-                time_axis = []
-                for i in range(data.shape[0]):
-                    time_axis.append(st + i * (1 / fs))
-                data_dict[tag] = (np.array(data), np.array(time_axis))
+                if fs < 0:
+                    data_dict[tag] = (np.array(data))
+                else:
+                    time_axis = []
+                    for i in range(data.shape[0]):
+                        time_axis.append(st + i * (1 / fs))
+                    data_dict[tag] = (np.array(data), np.array(time_axis))
 
         f.close()
         return data_dict
